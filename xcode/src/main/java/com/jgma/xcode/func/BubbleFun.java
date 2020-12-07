2f43d7bf712aa430a1f20b1fa2b3b554f69ca940
jgma_legion <18638691837@163.com>
2020-12-07 18:05:01 +0800

    冒泡沉底排法

--- xcode/src/main/java/com/jgma/xcode/func/BubbleFun.java
+++ xcode/src/main/java/com/jgma/xcode/func/BubbleFun.java
@@ -8,12 +8,14 @@ public class BubbleFun {
     public static void main(String[] args) {
 
         int[] array = new int[]{3, 1, 2, 5, 4, 8, 6, 45, 21, 0, 36, 56, 210, 254, 489, 1475, 124, 365, 88, 99};
+        int[] array2 = new int[]{3, 1, 2, 5, 4, 8, 6, 45, 21, 0, 36, 56, 210, 254, 489, 1475, 124, 365, 88, 99};
 
-        bubble1(array);
+        bubbleUp(array);
+        bubbleDown(array2);
 
     }
 
-    private static void bubble1(int[] array) {
+    private static void bubbleUp(int[] array) {
         long l = System.currentTimeMillis();
         // 遍历需要冒泡排序的数组，第一轮循环最小的在最前面
         for (int i = 0; i < array.length - 1; i++) {
@@ -30,6 +32,31 @@ public class BubbleFun {
                     int temp = array[j];
                     array[j] = array[i];
                     array[i] = temp;
+                }
+            }
+        }
+        System.out.println("耗时：" + (System.currentTimeMillis() - l));
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    private static void bubbleDown(int[] array) {
+        long l = System.currentTimeMillis();
+        // 遍历需要冒泡排序的数组，第一轮循环最小的在最前面
+        for (int i = 0; i < array.length - 1; i++) {
+            // 用此值与剩下的所有值比较
+            for (int j = 0; j < array.length -i -1; j++) {
+                try {
+                    Thread.sleep(1L);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                // 如果这个值大就把它放到最后面(沉底)
+                if (array[j] > array[j+1]){
+                    int temp = array[j];
+                    array[j]  = array[j+1];
+                    array[j+1] = temp;
                 }
             }
         }
