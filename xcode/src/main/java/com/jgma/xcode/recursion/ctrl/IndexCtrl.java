97824ad111e41776dd7c0f701f9f6bc728300743
majg <18638691837@163.com>
2022-11-23 09:35:10 +0800

    list递归树

--- xcode/src/main/java/com/jgma/xcode/recursion/ctrl/IndexCtrl.java
+++ xcode/src/main/java/com/jgma/xcode/recursion/ctrl/IndexCtrl.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 /**
  * @author JGMa
@@ -43,6 +46,24 @@ public class IndexCtrl {
             }
         }
         return departments;
+    }
+
+    /**
+     * list递归返回树
+     * @param root 根节点特征的数据
+     * @param all 原始数据list
+     * @return
+     */
+    public List<Map<String, Object>> getChildrens(Map root, List<Map<String, Object>> all) {
+        List<Map<String, Object>> children = all.stream().filter(m -> {
+            return Objects.equals(m.get("pid"), root.get("id"));
+        }).map(
+                (m) -> {
+                    m.put("children", getChildrens(m, all));
+                    return m;
+                }
+        ).collect(Collectors.toList());
+        return children;
     }
 
 }
